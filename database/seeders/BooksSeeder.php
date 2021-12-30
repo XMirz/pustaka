0d7fcd3242cbd179912bfe62ef8d26245c282c45
DenyArd <81862564+DenyArd@users.noreply.github.com>
2021-12-30 08:12:34 +0700

    12 buku

--- database/seeders/BooksSeeder.php
+++ database/seeders/BooksSeeder.php
@@ -20,7 +20,7 @@ class BooksSeeder extends Seeder
     public function run()
     {
         // Category
-        $name = ["Bahasa Indonesia", "Matematika", "Bahasa Inggris", "Filsafat", "Fiksi", "Umum"];
+        $name = ["Bahasa Indonesia", "Matematika", "Bahasa Inggris", "Filsafat", "Fiksi", "Agama", "Umum"];
         $code = ["BI", "MT", "EN", "FS", "FK", "UM"];
         for ($i = 0; $i < count($name); $i++) {
             $cat = new Category();
@@ -48,7 +48,35 @@ class BooksSeeder extends Seeder
         ]);
 // id =5
         Author::create([
-            'name' => 'Suerli, Maman Suryaman, Aji Septiaji, Istiqomah',
+            'name' => 'Suherli',
+        ]);
+// id =6
+        Author::create([
+            'name' => 'Nurhasanah',
+        ]);
+// id =7
+        Author::create([
+            'name' => 'Utami Widiati, Zuliati Rohmah, Furaidah',
+        ]);
+// id =8
+        Author::create([
+            'name' => 'Sukino',
+        ]);
+// id =9
+        Author::create([
+            'name' => 'Wono Setya Budhi',
+        ]);
+// id =10
+        Author::create([
+            'name' => 'H. M. Arifin',
+        ]);
+// id =11
+        Author::create([
+            'name' => 'Fuad Ihsan',
+        ]);
+// id =12
+        Author::create([
+            'name' => 'Imam Al Algazali',
         ]);
 
 
@@ -71,6 +99,26 @@ class BooksSeeder extends Seeder
         Publisher::create([
             'name' => 'Kemendikbud',
             'address' => 'Jl. Jenderal Sudirman, Senayan, Jakarta Pusat'
+        ]);
+// id =5
+        Publisher::create([
+            'name' => 'Erlangga',
+            'address' => 'Jl. H. Baping Raya No. 100 Ciracas Jakarta'
+        ]);
+// id =6
+        Publisher::create([
+            'name' => 'Bumi Aksara',
+            'address' => 'Jl. Sawo Raya No. 18 Rawamangun Jakarta'
+        ]);
+// id =7
+        Publisher::create([
+            'name' => 'Rineka Cipta',
+            'address' => 'Jl. Matraman Raya No. 148, Perkantoran Mitra Matraman Blok B. 1, RT.1/RW.4, Kb. Manggis, Kec. Matraman, Kota Jakarta Timur'
+        ]);
+// id =8
+        Publisher::create([
+            'name' => 'Taylor & Francis Inc.',
+            'address' => 'Milton Park, Abingdon-on-Thames, Oxfordshire Britania Raya'
         ]);
 
 // book
@@ -182,6 +230,158 @@ class BooksSeeder extends Seeder
             'category_id' => 1,
             'author_id' => 5,
             'publisher_id' => 4,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786024270995',
+            'title' => 'Bahasa Indonesia untuk SMA/MA/SMK/MAK Kelas X',
+            'title_description' => '',
+            'edition' => '2',
+            'publication_year' => '2016',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'290',
+            'amount' => 36,
+            'category_id' => 1,
+            'author_id' => 5,
+            'publisher_id' => 4,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786024271107',
+            'title' => 'Buku Guru Bahasa Inggris SMA/MA/SMK/MAK Kelas XI',
+            'title_description' => '',
+            'edition' => '2',
+            'publication_year' => '2017',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'160',
+            'amount' => 30,
+            'category_id' => 3,
+            'author_id' => 6,
+            'publisher_id' => 4,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786024271138',
+            'title' => 'Buku Guru Bahasa Inggris SMA/MA/SMK/MAK Kelas XII',
+            'title_description' => '',
+            'edition' => '2',
+            'publication_year' => '2018',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'168',
+            'amount' => 29,
+            'category_id' => 3,
+            'author_id' => 7,
+            'publisher_id' => 4,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786022987949',
+            'title' => 'MATEMATIKA UNTUK SMA/MA KELAS XII',
+            'title_description' => '',
+            'edition' => '1',
+            'publication_year' => '2018',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'252',
+            'amount' => 25,
+            'category_id' => 2,
+            'author_id' => 8,
+            'publisher_id' => 5,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786022989707',
+            'title' => 'Bupena Matematika Wajib Untuk SMA/MA Kelas X',
+            'title_description' => '',
+            'edition' => '1',
+            'publication_year' => '2014',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'161',
+            'amount' => 20,
+            'category_id' => 2,
+            'author_id' => 9,
+            'publisher_id' => 5,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9795260898',
+            'title' => 'Filsafat pendidikan Islam',
+            'title_description' => '',
+            'edition' => '1',
+            'publication_year' => '1994',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'192',
+            'amount' => 27,
+            'category_id' => 6,
+            'author_id' => 10,
+            'publisher_id' => 6,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+        
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9789795189817',
+            'title' => 'Filsafat Ilmu',
+            'title_description' => 'Perspektif Barat dan Islam',
+            'edition' => '1',
+            'publication_year' => '2010',
+            'published_at' => 'Jakarta',
+            'exemplar' =>'295',
+            'amount' => 25,
+            'category_id' => 4,
+            'author_id' => 11,
+            'publisher_id' => 7,
+        ]);
+        Stock::create([
+            'stock' => $book->amount,
+            'book_id' => $book->id
+        ]);
+
+        $book = Book::create([
+            'book_code' => Str::upper(Str::random(8)),
+            'isbn' => '9786023100002',
+            'title' => 'Kerancuan Filsafat',
+            'title_description' => 'Tahafut Al-Falasifah',
+            'edition' => '1',
+            'publication_year' => '2015',
+            'published_at' => 'Yogyakarta',
+            'exemplar' =>'372',
+            'amount' => 25,
+            'category_id' => 4,
+            'author_id' => 12,
+            'publisher_id' => 8,
         ]);
         Stock::create([
             'stock' => $book->amount,
