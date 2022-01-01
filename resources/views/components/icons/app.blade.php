800927fabc3c1931c727b1eee553405811c4af98
xmirz <linearattack0@gmail.com>
2022-01-01 13:40:59 +0700

    add security meta tag

--- resources/views/components/icons/app.blade.php
+++ resources/views/components/icons/app.blade.php
@@ -1,5 +1,4 @@
 @props(['class' => ''])
-<?xml version="1.0" encoding="UTF-8"?>
 <svg class="{{$class}}" version="1.1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"
   xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/"
   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
