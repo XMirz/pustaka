7c985741f5e9b8b9c2b6b7b94a7a274cd4e4bcb0
xmirz <linearattack0@gmail.com>
2022-01-01 20:51:55 +0700

    responsive login and main page

--- resources/views/components/cards/front-book.blade.php
+++ resources/views/components/cards/front-book.blade.php
@@ -1,6 +1,6 @@
 @props(['book'])
 <div href="{{route('books.index')}}" id=" total-books-card"
-  class="text-lg flex flex-row gap-4 flex-grow  border border-black/10 bg-white px-8 py-5 rounded-md shadow-sm">
+  class=" flex flex-row gap-4 flex-grow  border border-black/10 bg-white px-4 md:px-8 py-4 md:py-6 rounded-md shadow-sm">
   <div
     class=" overflow-hidden flex-shrink-0 h-fit w-24 md:w-32 aspect-[12/16] rounded bg-gray-300 flex items-center justify-center">
     @if($book->cover)
@@ -10,38 +10,38 @@
     @endif
   </div>
   <div class="w-full">
-    <h2 class="font-bold text-xl max-w-lg">{{$book->title}} {{$book->title_description != '' ? '-
+    <h2 class="font-bold text-base md:text-xl max-w-lg leading-5">{{$book->title}} {{$book->title_description != '' ? '-
       "'.$book->title_description.'"'
       :
       ''}}
     </h2>
-    <div class="mt-2 text-base md:text-lg leading-4">
-      <h5 class="">
+    <div class="mt-2 text-base md:text-lg ">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Penulis</span>: {{$book->author->name}}
       </h5>
-      <h5 class="">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Kategory</span>: {{$book->category->name}}
       </h5>
-      <h5 class="">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Penulis</span>: {{$book->isbn}}
       </h5>
-      <h5 class="flex max-w-lg">
+      <h5 class="leading-5 flex max-w-lg">
         <span class="w-20 md:w-36 flex-shrink-0">Terbitan</span> :&nbsp;<span class="">{{$book->publisher->name}},
           {{$book->published_at}}.
           Tahun
           {{$book->publication_year}}</span>
       </h5>
-      <h5 class="">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Jumlah</span>: {{$book->amount}} buku
       </h5>
-      <h5 class="">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Tempat</span>: Rak {{$book->category->place}}
       </h5>
-      <h5 class="">
+      <h5 class="leading-5">
         <span class="w-20 md:w-36 inline-flex">Jumlah halaman</span>: {{$book->exemplar}} halaman
       </h5>
-      <div class="w-full flex justify-between font-poppins font-semibold">
-        <h5>
+      <div class="w-full text-sm md:text-lg flex justify-between font-poppins font-semibold">
+        <h5 class="leading-5">
           <span class="w-20 md:w-36 inline-flex">Kode buku</span>: {{$book->book_code}}
         </h5>
         {!!$book->stock->stock > 0 ? '<h6 class="text-blue-600">Tersedia</h6>' : '<h6 class="text-red-500">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,9 @@
 <x-guest-layout>
-  <x-slot name="title">{{request('title') ? 'Cari Buku - ' : ''}}Perpustakaan SMK Telkom</x-slot>
-  <div class="relative h-screen overflow-y-auto flex flex-col  xl:pt-12">
+  <x-slot name="title">Masuk - Perpustakaan SMK Telkom</x-slot>
+  <div class="relative h-screen overflow-y-auto xl:pt-12 flex flex-col">
 
-    <div class="flex flex-col space-y-12 md:space-y-56">
-      <div class="px-8 xl:px-32 space-y-16">
+    <div class="flex flex-col  gap-y-12">
+      <div class="px-4 md:px-8 xl:px-32 gap-y-16">
         <div class="flex justify-start py-4 scale-95 md:scale-100">
           <a href="{{route('root')}}" class=" flex flex-row items-center space-x-4 justify-center ">
             <div class="w-14 h-14">
@@ -15,41 +15,40 @@
             </div>
           </a>
         </div>
-        <div class="">
-          <div class="w-full xl:w-1/2 font-poppins flex flex-col md:space-y-4">
-            <h1 id="texthero" class="relative font-bold text-3xl xl:text-7xl img-shadow">Sistem
-              Informasi Perpustakaan Digital
-            </h1>
-            <h3 class="font-medium  md:font-semibold text-2xl xl:text-4xl">SMK Telkom Pekanbaru</h3>
-            <div class="w-4/5 md:w-1/2 flex self-end md:absolute right-0 top-12 ">
-              <img class="img-shadow" src="{{asset('img/hero.webp')}}" alt="" srcset="">
-            </div>
-            <div class="w-full md:w-2/3 pt-8">
-              <form action="{{route('root')}}" method="GET">
-                <h4 class="text-blue-600 font-semibold text-lg md:text-2xl md:my-4">Cari buku</h4>
-                <div
-                  class="w-full py-2 px-4 md:px-6 border border-blue-500/10 shadow-2xl shadow-blue-500/30 rounded-md flex flex-row space-x-4">
-                  <div class="flex-grow">
-                    <label for="" class="text-blue-600 tracking-wider md:font-semibold">Judul buku</label>
-                    <input id="title" name="title" type="text" placeholder="Kata kunci"
-                      class="md:text-xl px-0 w-full border-0 focus:border-0 outline-none focus:outline-none focus:ring-0 bg-transparent"
-                      value="{{request('title') ?? ''}}" required />
-                  </div>
-                  <div class="flex items-center">
-                    <button type="submit">
-                      <x-icons.search size="8" class="text-blue-600" />
-                    </button>
-                  </div>
+        <div class="w-full md:w-3/5 xl:w-1/2 mt-4 md:mt-8 xl:mt-12 font-poppins flex flex-col md:space-y-4">
+          <h1 id="texthero" class="relative font-bold text-3xl md:text-5xl xl:text-7xl img-shadow">Sistem
+            Informasi Perpustakaan Digital
+          </h1>
+          <h3 class="font-medium  md:font-semibold text-2xl xl:text-4xl">SMK Telkom Pekanbaru</h3>
+          <div class="w-4/5 md:w-1/2 flex self-end md:absolute right-0 md:top-24 xl:top-12 ">
+            <img class="img-shadow" src="{{asset('img/hero.webp')}}" alt="" srcset="">
+          </div>
+          <div class="w-full md:w-2/3 pt-8">
+            <form action="{{route('root')}}" method="GET">
+              <h4 class="text-blue-600 font-semibold text-lg md:text-2xl md:my-4">Cari buku</h4>
+              <div
+                class="w-full py-2 px-4 md:px-6 border border-blue-500/10 shadow-2xl shadow-blue-500/30 rounded-md flex flex-row space-x-4">
+                <div class="flex-grow">
+                  <label for="" class="text-blue-600 tracking-wider md:font-semibold">Judul buku</label>
+                  <input id="title" name="title" type="text" placeholder="Kata kunci"
+                    class="md:text-xl px-0 w-full border-0 focus:border-0 outline-none focus:outline-none focus:ring-0 bg-transparent"
+                    value="{{request('title') ?? ''}}" required />
                 </div>
-              </form>
-            </div>
+                <div class="flex items-center">
+                  <button type="submit">
+                    <x-icons.search size="8" class="text-blue-600" />
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
 
       </div>
 
       @if(request('title'))
-      <div class="w-full bg-gray-100 flex flex-col font-sans space-y-4 md:space-y-8 px-8 xl:px-32  py-8 md:py-12 ">
+      <div
+        class="w-full bg-gray-100 flex flex-col font-sans space-y-4 md:space-y-8 px-4 md:px-8 xl:px-32  py-8 md:py-12 ">
         <h4 class="text-blue-600 font-poppins text-lg md:text-2xl font-semibold ">Hasil penelusuran</h4>
         @if($results->count() > 0)
         <div class="flex flex-row flex-wrap justify-between gap-4 md:gap-6">
@@ -59,24 +58,37 @@
         </div>
         @else
         <h4 class="font-poppins md:text-xl mx-6 text-center">Tidak ditemukan buku dengan judul
-          <span class="text-blue-600 font-semibold">{{request('title')}}!</span>
+          <span class="text-blue-600 font-semibold cya4">{{request('title')}}!</span>
         </h4>
         @endif
       </div>
       @endif
-      <div class="text-blue-500 {{request('title') ? 'pt-12' : 'pt-12 md:pt-32'}} opacity-50">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path fill="#0099ff" fill-opacity="1"
-            d="M0,160L40,176C80,192,160,224,240,234.7C320,245,400,235,480,197.3C560,160,640,96,720,101.3C800,107,880,181,960,181.3C1040,181,1120,107,1200,64C1280,21,1360,11,1400,5.3L1440,0L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z">
+    </div>
+    <div class=" opacity-100 mt-auto mb-0 ">
+      <div class="relative w-full max-h-48 xl:max-h-56 overflow-y-hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="">
+          <defs>
+            <linearGradient id="grad1" x1="100%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" style="stop-color:rgb(34, 211, 238);stop-opacity:0.7" />=
+              <stop offset="100%" style="stop-color:rgb(59, 130, 246);stop-opacity:1" />
+            </linearGradient>
+          </defs>
+          <path fill="url(#grad1)" fill-opacity="1"
+            d="M0,128L80,112C160,96,320,64,480,64C640,64,800,96,960,96C1120,96,1280,64,1360,48L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
           </path>
         </svg>
+        <div class="w-full h-16 bg-blue-500"></div>
+        <div class="absolute inset-0 flex flex-col justify-end text-white md:text-gray-800 font-poppins">
+          <div class="flex flex-col items-center justify-end pb-12 md:pb-8 md:text-lg">
+            <h3 class="font-semibold">Perpustakaan SMK Telkom - Pekanbaru</h3>
+            <h3>Copyright, Kelompok 2 <span class="font-semibold">&copy;</span> 2022</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-
   <x-slot name="script">
     <script>
-
     </script>
   </x-slot>
 </x-guest-layout>
